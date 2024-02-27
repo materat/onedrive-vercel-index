933b8a0aa6c9b44531108a6c3d60267a73e44485
materat <hackemate2@gmail.com>
2024-02-27 00:23:20 -0400

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // Prefix for KV Storage
-  kvPrefix: process.env.KV_PREFIX || '',
+  kvPrefix: process.env.KV_PREFIX || 'postgres',
 
   // The name of your website. Present alongside your icon.
   title: "Spencer's OneDrive",
